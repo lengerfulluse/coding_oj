255909176a7ab706a21436d5b9f64519db50b653
lengerfulluse <lengerfulluse@icloud.com>
2015-12-17 09:36:22 +0800

    Range Sum Query with forward algorithm.

--- src/me/hengwei/leetcode/NumArray_303.java
+++ src/me/hengwei/leetcode/NumArray_303.java
@@ -7,35 +7,37 @@ import java.util.Map;
  * https://leetcode.com/problems/range-sum-query-immutable/.
  */
 public class NumArray_303 {
-    Map<String, Integer> rangeMap = new HashMap<>();
+    int[] sum;
     int[] nums;
     public NumArray_303(int[] nums)
     {
         this.nums = nums;
+        sum = new int[nums.length];
+        if(nums.length == 0)
+        {
+            return;
+        }
+        sum[0] = nums[0];
+        for(int i=1; i<nums.length; i++)
+        {
+            sum[i] = sum[i-1] + nums[i];
+        }
     }
 
     public int sumRange(int i, int j)
     {
-        i = Math.max(i, 0);
-        j = Math.min(j, nums.length);
-        String key = String.valueOf(i)+":"+String.valueOf(j);
-        if(rangeMap.containsKey(key))
+        int start = Math.max(i, 0);
+        int end = Math.min(j, nums.length-1);
+        if(start > end)
         {
-            return rangeMap.get(key);
+            return 0;
         }
-        int tmp=i, sum=0;
-        while(tmp<=j)
-        {
-            sum += nums[tmp];
-            rangeMap.put(String.valueOf(i)+":"+String.valueOf(tmp), sum);
-            tmp++;
-        }
-        return sum;
+        return sum[end] - sum[start] + nums[start];
     }
 
     public static void main(String[] args)
     {
-        int[] nums = {-2, 0, 3, -5, 2, -1};
+        int[] nums = {};
         NumArray_303 range_sum_query_303 = new NumArray_303(nums);
         System.out.println(range_sum_query_303.sumRange(0, 2));
         System.out.println(range_sum_query_303.sumRange(2, 5));
