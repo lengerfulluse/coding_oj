3a0142b60aa28a2640734316d3c1209b961943c2
lengerfulluse <jj@hengwei.me>
2016-11-09 23:27:30 +0800

    add solution for quickSort with non-sequential top K.

--- src/me/hengwei/interview/QuickSortWithTopK.java
+++ src/me/hengwei/interview/QuickSortWithTopK.java
@@ -11,32 +11,30 @@ public class QuickSortWithTopK {
         if(array == null || array.length == 0 || k == 0) {
             return;
         }
-        quickSort(array, 0, array.length-1, k);
+        quickSortWithNonSequenceTopK(array, 0, array.length-1, k);
     }
 
-    public static void quickSort(int[] array, int start, int end, int k) {
+    public static void quickSortWithNonSequenceTopK(int[] array, int start, int end, int k) {
+        if (start == end && k == 1) {
+            System.out.println(array[start]);
+        }
         if (array == null || start >= end) {
             return;
         }
         int pivot = partition(array, start, end);
 
         if (end - pivot + 1 == k) {
-            System.out.println("got with exactly K size");
             for(int i = pivot; i<= end; i++) {
                 System.out.print(array[i] + "\t");
             }
-            System.out.println();
         } else if (end - pivot + 1 > k) {
-            System.out.println("got with larger than k size");
-            quickSort(array, pivot+ 1, end, k);
+            quickSortWithNonSequenceTopK(array, pivot+ 1, end, k);
         } else {
-            System.out.println("got with small than k size");
             /* we already know the biggest ones */
             for(int i = pivot; i<= end; i++) {
                 System.out.print(array[i] + "\t");
             }
-            System.out.println();
-            quickSort(array, start, pivot-1, k - (end - pivot + 1));
+            quickSortWithNonSequenceTopK(array, start, pivot-1, k - (end - pivot + 1));
         }
     }
 
@@ -64,9 +62,7 @@ public class QuickSortWithTopK {
 
     public static void main(String[] args) {
 
-        int[] array = new int[] {-1, 10, 8, 23, 11, 6, -2, 9, 0};
-        Arrays.stream(array).forEach(i->System.out.print(i + "\t"));
-        System.out.println();
+        int[] array = new int[] {-1, 10, 8, 23, 78, 11, 6, -2, 9, 0, 100};
         int k = 3;
         QuickSortWithTopK.quickSortWithTopK(array, k);
     }
