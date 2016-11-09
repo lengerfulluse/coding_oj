b97768d06d2dba2cf5e2eb072f21b8b90062b844
lengerfulluse <jj@hengwei.me>
2016-11-09 23:18:19 +0800

    fix one-way quick sort bug.

--- src/me/hengwei/interview/QuickSortWithTopK.java
+++ src/me/hengwei/interview/QuickSortWithTopK.java
@@ -19,25 +19,44 @@ public class QuickSortWithTopK {
             return;
         }
         int pivot = partition(array, start, end);
-        quickSort(array, start, pivot-1, k);
-        quickSort(array, pivot+1, end, k);
+
+        if (end - pivot + 1 == k) {
+            System.out.println("got with exactly K size");
+            for(int i = pivot; i<= end; i++) {
+                System.out.print(array[i] + "\t");
+            }
+            System.out.println();
+        } else if (end - pivot + 1 > k) {
+            System.out.println("got with larger than k size");
+            quickSort(array, pivot+ 1, end, k);
+        } else {
+            System.out.println("got with small than k size");
+            /* we already know the biggest ones */
+            for(int i = pivot; i<= end; i++) {
+                System.out.print(array[i] + "\t");
+            }
+            System.out.println();
+            quickSort(array, start, pivot-1, k - (end - pivot + 1));
+        }
     }
 
     public static int partition(int[] array, int start, int end) {
-        int pos = start, pivotValue = array[start];
-        int pivot = start;
-        for(int i=start + 1; i<= end; i++) {
-            if(array[i] < pivotValue) {
+        int pos = start+1, pivotValue = array[start];
+        for(int i=start+1; i<= end; i++) {
+            if(array[i] <= pivotValue) {
                 swap(array, pos, i);
-                pivot = i;
                 pos++;
             }
         }
-        swap(array, pivot, pos);
+        pos--;
+        swap(array, start, pos);
         return pos;
     }
 
     public static void swap(int[] array, int i, int j) {
+        if (i == j) {
+            return;
+        }
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
@@ -45,9 +64,10 @@ public class QuickSortWithTopK {
 
     public static void main(String[] args) {
 
-        int[] array = new int[] {-1, 10, 8, 6, -2, 9, 0};
+        int[] array = new int[] {-1, 10, 8, 23, 11, 6, -2, 9, 0};
+        Arrays.stream(array).forEach(i->System.out.print(i + "\t"));
+        System.out.println();
         int k = 3;
         QuickSortWithTopK.quickSortWithTopK(array, k);
-        Arrays.stream(array).forEach((i)->System.out.print(i + "\t"));
     }
 }
